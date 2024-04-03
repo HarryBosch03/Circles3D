40acd9ad5cdc24f8d46d61e6b1a01246acd10f2c
HarryBosch <harrybosch03@gmail.com>
2024-04-03 21:52:27 +1100

    The Sex Update, Small Patch

--- Assets/Runtime/Util/NetworkTestUI.cs
+++ Assets/Runtime/Util/NetworkTestUI.cs
@@ -2,11 +2,15 @@ using System.Diagnostics;
 using FishNet;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Runtime.Util
 {
     public class NetworkTestUI : MonoBehaviour
     {
+        [FormerlySerializedAs("targetIP")] public string address = "localhost";
+        public ushort targetPort = 8888;
+        
         private void OnGUI()
         {
             if (!InstanceFinder.NetworkManager.IsOffline) return;
@@ -39,13 +43,13 @@ namespace Runtime.Util
 
         private void StartHost()
         {
-            InstanceFinder.ServerManager.StartConnection();
+            InstanceFinder.ServerManager.StartConnection(targetPort);
             StartClient();
         }
 
         private void StartClient()
         {
-            InstanceFinder.ClientManager.StartConnection();
+            InstanceFinder.ClientManager.StartConnection(address, targetPort);
         }
         
         private void StartNinja()
